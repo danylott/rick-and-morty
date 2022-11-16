da767cf29c7d913fcb253fc9065be36ed9a66844
danylott <d.tuytuyshkin@gmail.com>
2022-11-16 14:12:50 +0200

    Fix quotes in settings

--- rick_and_morty_api/settings.py
+++ rick_and_morty_api/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = "rick_and_morty_api.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
