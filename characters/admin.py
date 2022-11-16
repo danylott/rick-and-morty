dd5866177d6c395804488fe59507059053574d7b
danylott <d.tuytuyshkin@gmail.com>
2022-11-16 14:29:10 +0200

    Added models & migrations

--- characters/admin.py
+++ characters/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from characters.models import Character
+
+admin.site.register(Character)
--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    class StatusChoices(models.TextChoices):
+        ALIVE = "Alive"
+        DEAD = "Dead"
+        UNKNOWN = "unknown"
+
+    class GenderChoices(models.TextChoices):
+        FEMALE = "Female"
+        MALE = "Male"
+        GENDERLESS = "Genderless"
+        UNKNOWN = "unknown"
+
+    api_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=255)
+    status = models.CharField(max_length=50, choices=StatusChoices.choices)
+    species = models.CharField(max_length=255)
+    gender = models.CharField(max_length=50, choices=GenderChoices.choices)
+    image = models.URLField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
